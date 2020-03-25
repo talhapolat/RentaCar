679b64d5251fc6edc75653421722a01d83baaf9e
talhapolat <talhapolat@hotmail.com>
2020-03-25 22:19:05 +0300

    Completed until part 8.

--- product/models.py
+++ product/models.py
@@ -47,6 +47,7 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
     image_tag.short_description = 'Image'
